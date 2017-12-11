ed9f0c844de192ca1e823cbf80e8bfd99e8cf9d1
tanishi109 <note109@gmail.com>
2017-12-11 18:19:49 +0900

    add channel handler

--- lib/four_fingers_server_web/channels/room_channel.ex
+++ lib/four_fingers_server_web/channels/room_channel.ex
@@ -2,9 +2,15 @@ defmodule FourFingersServerWeb.RoomChannel do
   use Phoenix.Channel
 
   def join("room:lobby", _message, socket) do
+    IO.puts("*** room:lobby");
     {:ok, socket}
   end
   def join("room:" <> _private_room_id, _params, _socket) do
     {:error, %{reason: "unauthorized"}}
+  end
+
+  def handle_in("new_msg", %{"body" => body}, socket) do
+    broadcast! socket, "new_msg", %{body: body}
+    {:noreply, socket}
   end
 end
